fd6ac612dccb00a75832223eb3895244c45a6f7e
Katherine Hambley <superdesigngirl@mac.com>
2021-09-13 12:29:53 -0500

    Add project files.

---
+++ GetAllBinaryStrings/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GetAllBinaryStrings
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(get_all_binary_strings(3));
+		}
+		public static List<List<string>> get_all_binary_strings(int n)
+		{
+			List<List<string>> result = new List<List<string>>();
+
+			List<string> slate = new List<string>();
+
+			helper(n, slate, result);
+
+			return result;
+		}
+		public static void helper(int n, List<string> slate, List<List<string>> result)
+		{
+			if(slate.Count <= n)
+			{
+				if (n == slate.Count)
+				{
+					result.Add(slate);
+				}
+				else
+				{
+					slate.Add("0");
+					helper(n, slate, result);
+					slate.Add("1");
+					helper(n, slate, result);
+				}
+			}
+			
+		}
+		
+	}
+}
